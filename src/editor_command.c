8451ffbe17dfde9877757b5470cb57e6a292e3fb
Hossin-Azmoud <azmoud98@gmail.com>
2024-04-15 17:01:47 +0100

    refactored the command execution and fixed branchings that might lead to a buy

--- src/editor_command.c
+++ src/editor_command.c
@@ -30,6 +30,29 @@ static void editor_command_execute_normal(MiEditor *E, char *command)
     }
 }
 
+static void editor_cd_internal(eCommand *cmd, MiEditor *E) {
+    // Performs the cd command.
+    if (cmd->size) {
+        BrowseEntryT type = get_entry_type(cmd->argv[0]);
+        switch (type) {
+        case DIR__: {
+            // Here we change the directory.
+            E->fb   = realloc_fb(E->fb, cmd->argv[0], E->renderer->win_h);
+        } break;
+        case NOT_EXIST: {
+            // We say that it does not exist.
+            sprintf(E->notification_buffer, "the arg distenation does not exist.");
+        } break;
+        case FILE__: {
+            // We just say that it is an illegal dir type.
+            sprintf(E->notification_buffer, "Expected directory not a file as arg.");
+        } break;
+        default: {
+            sprintf(E->notification_buffer, "Illegal case at (editor_command_execute_fb)");
+        } break;
+        }
+    } 
+}
 
 static void editor_command_execute_fb(MiEditor *E, char *command) 
 {
@@ -39,11 +62,13 @@ static void editor_command_execute_fb(MiEditor *E, char *command)
             case KEY_SAVE_: {
                 // TODO: Save The whole file.
                 sprintf(E->notification_buffer, "(*) The editor can not save in the FILE BROWSER mode.");
+                return;
             } break;
             case KEY_QUIT:
             case KEY_QUIT_SAVE: {
                 // TODO: Save, Exit.
                 E->exit_pressed = true;
+                return;
             } break;
             default: {};
         }
@@ -55,30 +80,22 @@ static void editor_command_execute_fb(MiEditor *E, char *command)
         // TODO: make an prexesting command to execute inside the editor.
         // NOTE: since we have the editor, maybe I should call the editor_changedir(E, dir)
         // NOTE: Before doing anything I need to check if the operand is a directory.
-        if (cmd->size) {
-            BrowseEntryT type = get_entry_type(cmd->argv[0]);
-            switch (type) {
-            case DIR__: {
-                // Here we change the directory.
-                E->fb   = realloc_fb(E->fb, cmd->argv[0], E->renderer->win_h);
-            } break;
-            case NOT_EXIST: {
-                // We say that it does not exist.
-                sprintf(E->notification_buffer, "the arg distenation does not exist.");
-            } break;
-            case FILE__: {
-                // We just say that it is an illegal dir type.
-                sprintf(E->notification_buffer, "Expected directory not a file as arg.");
-            } break;
-            default: {
-                sprintf(E->notification_buffer, "Illegal case at (editor_command_execute_fb)");
-            } break;
-            }
-        }
+        editor_cd_internal(cmd, E);
+        return;
     }
+
+    if (strcmp(cmd->name, "e") == 0) {
+        // TODO: make
+        sprintf(E->notification_buffer, "(*) Edit command was trigerred");
+        return;
+    }
+    
+    sprintf(E->notification_buffer, 
+        "unknowm command: %s", cmd->name);
 
     command_distroy(cmd);
 }
+
 
 void editor_command_execute(MiEditor *E, char *command, editorMode mode) {
     switch (mode) {
